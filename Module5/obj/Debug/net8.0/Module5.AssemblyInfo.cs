70b4302d72fc814acaa990133197e241c427cdcb
v.sedlachek <v.sedlachek@dodobrands.io>
2024-09-03 14:08:02 +0300

    first commit

--- Module5/obj/Debug/net8.0/Module5.AssemblyInfo.cs
+++ Module5/obj/Debug/net8.0/Module5.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Module5")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0a903afd11107d1fb8733876daa3c5926272968c")]
 [assembly: System.Reflection.AssemblyProductAttribute("Module5")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Module5")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
